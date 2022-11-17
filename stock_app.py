519727e2ff62fe236aa71330db43e7a9cbf17817
Shiyu95 <hshiyu95@gmail.com>
2022-11-17 10:08:45 -0800

    Take a sample of a dataset to make stock price predictions using the LSTM model

--- stock_app.py
+++ stock_app.py
@@ -1,3 +1,5 @@
+#Build the dashboard using Plotly dash
+
 #Importing the Libraries
 from dash import Dash, html, dcc  # importing the dash framework
 from dash import dcc # importing the core components from dash
@@ -32,28 +34,35 @@ train=dataset[0:987,:]
 valid=dataset[987:,:]
 scaler=MinMaxScaler(feature_range=(0,1))
 scaled_data=scaler.fit_transform(dataset)
+#split the data into x_train and y_train dataset
 x_train,y_train=[],[]
 for i in range(60,len(train)):
     x_train.append(scaled_data[i-60:i,0])
     y_train.append(scaled_data[i,0])
-    
+#convert x_train and y_train to numpy arrays
 x_train,y_train=np.array(x_train),np.array(y_train)
+#reshape the data to 3 dimension
 x_train=np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
 model=load_model("saved_model.h5")
 inputs=new_data[len(new_data)-len(valid)-60:].values
 inputs=inputs.reshape(-1,1)
 inputs=scaler.transform(inputs)
+#create dataset x_test
 X_test=[]
 for i in range(60,inputs.shape[0]):
     X_test.append(inputs[i-60:i,0])
+#convert data to numpy array
 X_test=np.array(X_test)
+#reshape the data
 X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+#get the models predicted price values
 closing_price=model.predict(X_test)
 closing_price=scaler.inverse_transform(closing_price)
 train=new_data[:987]
 valid=new_data[987:]
 valid['Predictions']=closing_price
 df= pd.read_csv("stock_data.csv")
+
 # Data Visualization With Graphs
 # set app layout
 app.layout = html.Div([
@@ -64,6 +73,7 @@ app.layout = html.Div([
         dcc.Tab(label='NSE-TATAGLOBAL Stock Data',children=[
             html.Div([
                 html.H2("Actual closing price",style={"textAlign": "center"}),
+
                 dcc.Graph(
                     id="Actual Data",
                     figure={
@@ -79,6 +89,7 @@ app.layout = html.Div([
                             xaxis={'title':'Date'},
                             yaxis={'title':'Closing Rate'}
                         )
+
                     }
                 ),
                 html.H2("LSTM Predicted closing price",style={"textAlign": "center"}),
@@ -115,7 +126,7 @@ app.layout = html.Div([
                              style={"display": "block", "margin-left": "auto", 
                                     "margin-right": "auto", "width": "60%"}),
                 dcc.Graph(id='highlow'),
-                html.H1("Facebook Market Volume", style={'textAlign': 'center'}),
+                html.H1("Company Market Volume", style={'textAlign': 'center'}),
          
                 dcc.Dropdown(id='my-dropdown2',
                              options=[{'label': 'Tesla', 'value': 'TSLA'},
